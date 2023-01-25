ddf1051e7566aade5afdc9a8fc3ab2dd5de584a4
xfqwdsj <xfqwdsj@qq.com>
2023-01-25 18:24:45 +0800

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -61,8 +61,6 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    platform = api.platform();
-    isRelease = api.rustReleaseMode();
   }
 
   @override
@@ -74,38 +72,8 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text("You're running on"),
-            FutureBuilder<List<dynamic>>(
-              future: Future.wait([platform, isRelease]),
-              builder: (context, snap) {
-                final style = Theme.of(context).textTheme.headline4;
-                if (snap.error != null) {
-                  debugPrint(snap.error.toString());
-                  return Tooltip(
-                    message: snap.error.toString(),
-                    child: Text('Unknown OS', style: style),
-                  );
-                }
-
-                final data = snap.data;
-                if (data == null) return const CircularProgressIndicator();
-
-                final Platform platform = data[0];
-                final release = data[1] ? 'Release' : 'Debug';
-                final text = const {
-                      Platform.Android: 'Android',
-                      Platform.Ios: 'iOS',
-                      Platform.MacApple: 'MacOS with Apple Silicon',
-                      Platform.MacIntel: 'MacOS',
-                      Platform.Windows: 'Windows',
-                      Platform.Unix: 'Unix',
-                      Platform.Wasm: 'the Web',
-                    }[platform] ??
-                    'Unknown OS';
-                return Text('$text ($release)', style: style);
-              },
-            )
+          children: const <Widget>[
+            Text("Ni hao."),
           ],
         ),
       ),
